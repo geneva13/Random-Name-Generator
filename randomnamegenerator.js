e6132f6b1fae9ce7c7face119dcf1b3b6f58b6df
Geneva Jacobs <144170448+geneva13@users.noreply.github.com>
2024-03-04 16:37:43 +0000

    generates names

--- randomnamegenerator.js
+++ randomnamegenerator.js
@@ -1,5 +1,5 @@
 
-/*
+/*VIDEO FUNCTION TUTORIAL EXAMPLES
 function click0matic() {
     alert('Hello');
 }
@@ -12,9 +12,27 @@ window.onload = function() {
     });
 }
 */
-const names = ['John', 'Jane', 'Alice', 'Bob', 'Charlie', 'Emma', 'David', 'Ella'];
 
-function generateRandomName() {
-    const randomIndex = Math.floor(Math.random() * names.length);
-    return names[randomIndex];
-}
+
+// list the names and create a function and returns random output
+//remove export function?
+//get element by ID?
+
+//const names = ['John', 'Jane', 'Alice', 'Bob', 'Charlie', 'Emma', 'David', 'Ella'];
+
+//export function generateRandomName() {
+    //const randomIndex = Math.floor(Math.random() * names.length);
+    //return names[randomIndex];
+//}
+
+/*function rando() {
+    const names = ['Olivia', 'Noah', 'Emma', 'Liam', 'Amelia', 'Oliver', 'Sophia', 'Elijah', 'Charlotte', 'Mateo'];
+    return {
+      get name() {
+        Math.floor(Math.random() * max)
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        return names[Math.floor(Math.random() * names.length)];
+      },
+    }
+  }
+  */
